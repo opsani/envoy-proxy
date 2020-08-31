bdeac1b30afd837362aa68a82256ed6e37d9cc93
Blake Watters <blake@opsani.com>
2020-08-31 03:00:42 -0700

    Drop unnecessary logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,7 @@ export SERVICE_PORT=${SERVICE_PORT:-80}
 export METRICS_PORT=${METRICS_PORT:-9901}
 
 echo "Generating envoy.yaml config file..."
-env
 cat /tmpl/envoy.yaml.tmpl | envsubst > /etc/envoy.yaml
-cat /etc/envoy.yaml
 
 echo "Starting Envoy..."
 /usr/local/bin/envoy -c /etc/envoy.yaml
