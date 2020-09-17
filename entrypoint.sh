e4d75f0f542c679a33e4f4ab61471a76ed5129c0
Blake Watters <blake@opsani.com>
2020-09-16 19:20:45 -0700

    Update to match edits in Opsani Dev ref config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-export LISTEN_PORT=${LISTEN_PORT:-8080}
-export SERVICE_PORT=${SERVICE_PORT:-80}
-export METRICS_PORT=${METRICS_PORT:-9901}
+export OPSANI_ENVOY_PROXY_SERVICE_PORT=${OPSANI_ENVOY_PROXY_SERVICE_PORT:-80}
+export OPSANI_ENVOY_PROXIED_CONTAINER_PORT=${OPSANI_ENVOY_PROXIED_CONTAINER_PORT:-8080}
+export OPSANI_ENVOY_METRICS_PORT=${OPSANI_ENVOY_METRICS_PORT:-9901}
 
 echo "Generating envoy.yaml config file..."
 cat /tmpl/envoy.yaml.tmpl | envsubst > /etc/envoy.yaml
